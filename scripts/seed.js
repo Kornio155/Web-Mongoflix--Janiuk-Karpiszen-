f11d0c747b3738b47f39209e3f2cb97167b0be30
sigma <asdasda@gmail.com>
2026-02-24 14:26:13 +0100

    Finish and test seeding

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,1 +1,17 @@
+db = db.getSiblingDB('mongoflix');
 
+db.movies.drop()
+
+console.log("--- Start: Inicjalizacja bazy Mongoflix ---");
+
+const fs = require("fs")
+const path = require("path")
+
+const filePath = path.join(__dirname, "../data/movies.json")
+
+const fileContent = fs.readFileSync(filePath, "utf8")
+const data = JSON.parse(fileContent)
+
+db.movies.insertMany(data)
+
+print("--- Sukces: Inicjalizacja przebiegła pomyślnie! ---")
